7d18d04ce66186c2412753b5bf6a5d7fa096fb3e
Jen-Wei Hu <jenweihu@gmail.com>
2015-01-22 15:28:29 +0800

    Fix the problem when the virtual network is already started. And fix that OVX does not update the status when stopping this instance.

--- src/main/java/net/onrc/openvirtex/api/service/handlers/tenant/StartOVXNetwork.java
+++ src/main/java/net/onrc/openvirtex/api/service/handlers/tenant/StartOVXNetwork.java
@@ -52,16 +52,17 @@ public class StartOVXNetwork extends ApiHandler<Map<String, Object>> {
             final OVXNetwork virtualNetwork = map.getVirtualNetwork(tenantId
                     .intValue());
             
-			if (virtualNetwork.isBooted()) {
-				this.log.info("This virtual network {} has already started",
-						virtualNetwork.getTenantId());
-			} else {
+            if (virtualNetwork.isBooted()) {
+            	this.log.info("This virtual network {} has already started",
+            			virtualNetwork.getTenantId());
+        	} else {
 				virtualNetwork.boot();
 				this.log.info("Booted virtual network {}",
 						virtualNetwork.getTenantId());
-			}
-			Map<String, Object> reply = new HashMap<String, Object>(
+            }
+            Map<String, Object> reply = new HashMap<String, Object>(
 					virtualNetwork.getDBObject());
+			reply.put(TenantHandler.IS_BOOTED, virtualNetwork.isBooted());
 			resp = new JSONRPC2Response(reply, 0);
 
         } catch (final MissingRequiredField e) {
