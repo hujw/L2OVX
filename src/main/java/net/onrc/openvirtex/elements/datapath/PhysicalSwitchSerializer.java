ac9b4d754d2f79fdbda9e0eac146876e44e12465
Jen-Wei Hu <jenweihu@gmail.com>
2015-09-14 16:40:27 +0800

    Modify the JSON response format of API GetPhysicalTopology. The original JSON message is "switches: [00:00:00:00:00:00:00:01, 00:00:00:00:00:00:00:02]". After modifying, the return JSON is changed to "switches: [{"dpid": "00:00:00:00:00:00:00:01"}, {"dpid": "00:00:00:00:00:00:00:02"}]"

--- src/main/java/net/onrc/openvirtex/elements/datapath/PhysicalSwitchSerializer.java
+++ src/main/java/net/onrc/openvirtex/elements/datapath/PhysicalSwitchSerializer.java
@@ -18,6 +18,7 @@ package net.onrc.openvirtex.elements.datapath;
 import java.lang.reflect.Type;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
@@ -27,8 +28,9 @@ public class PhysicalSwitchSerializer implements JsonSerializer<PhysicalSwitch> 
     @Override
     public JsonElement serialize(final PhysicalSwitch sw,
             final Type switchType, final JsonSerializationContext context) {
-        final JsonPrimitive dpid = new JsonPrimitive(sw.switchName);
-        return dpid;
+    	final JsonObject result = new JsonObject();
+    	result.addProperty("dpid", sw.switchName);
+        return result;
     }
 
 }
