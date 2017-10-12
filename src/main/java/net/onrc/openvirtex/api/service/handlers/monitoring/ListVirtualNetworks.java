1381c9612afe3b482b4ed5a047816939d8e831b1
hujw <jenweihu@gmail.com>
2017-10-12 10:38:28 +0800

    Set more detail information for the "listVirtualNetworks" command

--- src/main/java/net/onrc/openvirtex/api/service/handlers/monitoring/ListVirtualNetworks.java
+++ src/main/java/net/onrc/openvirtex/api/service/handlers/monitoring/ListVirtualNetworks.java
@@ -15,14 +15,22 @@
  ******************************************************************************/
 package net.onrc.openvirtex.api.service.handlers.monitoring;
 
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import net.onrc.openvirtex.api.service.handlers.ApiHandler;
 import net.onrc.openvirtex.elements.OVXMap;
+import net.onrc.openvirtex.elements.datapath.PhysicalSwitch;
+import net.onrc.openvirtex.elements.datapath.PhysicalSwitchSerializer;
 import net.onrc.openvirtex.elements.network.OVXNetwork;
+import net.onrc.openvirtex.elements.port.PhysicalPort;
+import net.onrc.openvirtex.elements.port.PhysicalPortSerializer;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2ParamsType;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
 
@@ -36,11 +44,30 @@ public class ListVirtualNetworks extends ApiHandler<Object> {
     @Override
     public JSONRPC2Response process(final Object params) {
         JSONRPC2Response resp = null;
-
+        
+        final GsonBuilder gsonBuilder = new GsonBuilder();
+        // gsonBuilder.setPrettyPrinting();
+        gsonBuilder.excludeFieldsWithoutExposeAnnotation();
+        gsonBuilder.registerTypeAdapter(PhysicalSwitch.class,
+                new PhysicalSwitchSerializer());
+        
+        final Gson gson = gsonBuilder.create();
+        
         final Map<Integer, OVXNetwork> nets = OVXMap.getInstance()
                 .listVirtualNetworks();
-        // JSONRPC2Response wants a List, not a Set
-        final List<Integer> list = new ArrayList<Integer>(nets.keySet());
+        final List<Map<String, Object>> list = new LinkedList<Map<String, Object>>();
+        
+        for (OVXNetwork n: nets.values()) {
+        	Map<String, Object> result = gson.fromJson(gson.toJson(n), Map.class);
+        	Map<String, Object> map = new LinkedHashMap<String, Object>();
+        	map.put("tenantId", n.getTenantId());
+        	map.put("ctrl_ip", n.getControllerUrls());
+        	map.put("booted", n.isBooted());
+        	map.put("status", n.getStatus());
+        	map.put("details", n.getStatusDetails());
+        	list.add(map);
+        }
+//        final List<Integer> list = new ArrayList<Integer>(nets.keySet());
         resp = new JSONRPC2Response(list, 0);
         return resp;
     }
