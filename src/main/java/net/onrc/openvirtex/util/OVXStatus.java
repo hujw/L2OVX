c903544b59431d0e5e11ee7d1cff176eae4f7a78
hujw <jenweihu@gmail.com>
2017-10-20 14:05:20 +0800

    The display messages and status on web

---
+++ src/main/java/net/onrc/openvirtex/util/OVXStatus.java
@@ -0,0 +1,13 @@
+package net.onrc.openvirtex.util;
+
+public class OVXStatus {
+	public static String UP = "up";
+	public static String WARN = "warn";
+	public static String ERROR = "error";
+	
+	public static String BOOTED = "Booted";
+	public static String NOT_BOOTED = "Not booted";
+	public static String LINK_UP = "link up";
+	public static String LINK_DOWN = "link down";
+	public static String SWITCH_TO_BACKUP = "Switch to the BACKUP path";
+}
